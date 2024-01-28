c1e867aad07ac238e21a892ca1283b7ffa5b6f6d
woontoon <wantanrangian@protonmail.ch>
2024-01-29 00:07:16 +1100

    1/2 commits - finishing up

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LesswrongLink from "@/components/profile/LesswrongLink";
+import LesswrongCreate from "@/components/profile/LesswrongCreate";
 
 const CreateProfilePage = () => {
   return (
@@ -10,7 +10,7 @@ const CreateProfilePage = () => {
       <p className="text-center mb-4">
         Connect with LessWrong to create your profile.
       </p>
-      <LesswrongLink />
+      <LesswrongCreate />
     </div>
   );
 };
