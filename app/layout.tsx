fe6f9c5b25ad10eca6525bc13238dd396f396142
Quinn Dougherty <quinnd@riseup.net>
2024-02-01 10:13:15 -0800

    hotfix: Footer in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="min-h-screen flex flex-col items-center">
           <Header />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -52,14 +52,6 @@ export default async function Index() {
         ))}
       </div>
       <AllPostsList />
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <a
-          href="https://github.com/quinn-dougherty/rallypoint"
-          className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-        >
-          <Image src="/github.png" alt="GitHub" height="30" width="30" />
-        </a>
-      </footer>
     </div>
   );
 }
